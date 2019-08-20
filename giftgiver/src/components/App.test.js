c7f8be9349ed8883aa82f8bc751c22f97a22a456
George701 <chernikov.georgii@gmail.com>
2019-08-20 15:14:55 +0300

    Behaviour driven development for App.test.js

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { shallow } from '../enzyme';
 import App from './App';
 
-const app = shallow(<App/>);
+describe('App', () => {
+    const app = shallow(<App/>);
 
-it('renders correctly', ()=>{
-    expect(app).toMatchSnapshot();
-});
+    it('renders correctly', ()=>{
+        expect(app).toMatchSnapshot();
+    });
 
-it('initializes the `state` with an empty list of gifts', ()=>{
-    expect(app.state().gifts).toEqual([]);
-});
+    it('initializes the `state` with an empty list of gifts', ()=>{
+        expect(app.state().gifts).toEqual([]);
+    });
 
-it('adds a new gift to `state` when clicking the add button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{id: 1}]);
-});
+    describe('when clicking the `add-gift` button', () => {
 
-it('adds new gift to the rendered list when clicking `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(app.state().gifts.length);
+        beforeEach(()=>{
+            app.find('.btn-add').simulate('click');
+        });
+
+        afterEach(()=>{
+            app.setState({gifts: []});
+        });
+
+        it('adds a new gift to `state`', () => {
+            expect(app.state().gifts).toEqual([{id: 1}]);
+        });
+    
+        it('adds new gift to the rendered list', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    });
 });
