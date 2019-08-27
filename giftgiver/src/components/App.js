1f94551bbcc4da1887d79b5d2d48ad8016d3fea6
George701 <chernikov.georgii@gmail.com>
2019-08-27 21:31:53 +0300

    Coverage test and helper functions

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 import Gift from './Gift';
+import { max_number } from '../helper'
+
+//coverage test command
+// npm run test -- --coverage --watchALL
 
 class App extends Component {
     constructor(){
@@ -33,9 +37,7 @@ class App extends Component {
 
     addGift = () => {
         const { gifts } = this.state;
-        const ids = this.state.gifts.map(gift => gift.id);
-        const max_id = ids.length > 0 ? Math.max(...ids) : 0;
-        gifts.push({id: max_id+1});
+        gifts.push({id: max_number(this.state.gifts.map(gift => gift.id))+1});
         this.setState({ gifts });
     };
 
