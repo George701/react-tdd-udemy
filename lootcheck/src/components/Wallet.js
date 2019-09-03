54d3b43a2f8b0ae31b258db4e8f935da065ade25
George701 <chernikov.georgii@gmail.com>
2019-09-03 15:00:13 +0300

    Local Wallet Balance with State

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+    constructor() {
+        super();
+        this.state = {
+            balance: ''
+        }
+    }
     render() {
         return (
             <div>
                 <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+                <br />
+                <input className="input-wallet" type='text' onChange={this.updateBalance} />
             </div>
         )
+    }
+
+    updateBalance = e => {
+        this.setState({ balance: parseInt(e.target.value, 10) })
     }
 }
 
